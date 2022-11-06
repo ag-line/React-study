56c54aa99a089b65418f66e40767d6f476f8544e
dmstjs8278 <dmstjs8278@naver.com>
2022-11-06 12:00:30 +0900

    React-Study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [제목, 제목변경] = useState(["강남", "용산", "인천"]); //array[값,수정함수]
+  let [cnt, cnt_revise] = useState(0);
+  let day = "2022-11-04";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-Nav">Area</div>
+      <div className="list">
+        <h3>
+          {제목[0]}
+          <span
+            onClick={() => {
+              cnt_revise(cnt + 1);
+            }}
+          >
+            👍 {cnt}
+          </span>
+        </h3>
+        <p>{day}</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{제목[1]}</h3>
+        <p>{day}</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{제목[2]}</h3>
+        <p>{day}</p>
+        <hr />
+      </div>
+
+      <Modal />
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="Modal">
+      <h3>제-목</h3>
+      <p>yy-mm-dd</p>
+      <p>상세내용</p>
+      <hr />
     </div>
   );
 }
